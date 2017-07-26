cd75518d9974e937d50cad230be6bb1d9835ee71
Sean Yalda <seanyalda@gmail.com>
2017-07-25 23:50:29 -0700

    seperated interface code into its own file

---
+++ js/interfaceCode.js
@@ -0,0 +1,43 @@
+// Listen for clicks outside of opened <div> elements 
+$(document).ready(function(){
+	$(document).click(function(e){
+		if ($(e.target).is('#contact,#contact *, #navigation, #navigation *, #about.visible, #about.visible *')) {
+			return;
+		}
+		else {
+			$("#contact").removeClass('visible');
+			$("#about").removeClass('visible'); 
+		}
+	});
+});
+
+// Opens #contact <div> on 'Contact' menu click
+$(".clickable.contact").on('click', function(){
+	$("#contact").addClass('visible'); 
+});
+
+// Opens #about <div> on 'About' menu click 
+$(".clickable.about").on('click', function(){
+	$("#about").addClass('visible'); 
+});
+
+// Opens #coming <div> on 'Shop' menu click, remove after 4 seconds -->
+$(".clickable.shop").on('click', function(){
+	$("#coming").addClass('visible');
+	window.setTimeout(function(){
+		$("#coming").removeClass('visible');
+	}, 3000);
+});
+
+// Toggles #navigation <div> on 'Open Mike Eagle' menu click -->
+$("h1").on('click', function(){
+	$("#navigation").toggleClass('visible'); 
+});
+
+// Remove .visible on all possible open <div> elements -->
+$(".close").on('click', function(){
+	$("#contact").removeClass('visible');
+	$("#about").removeClass('visible'); 
+	$("#coming").removeClass('visible');
+	$("#navigation").removeClass('visible');
+});
